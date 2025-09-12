7eb27f10673c7310861b1ebc41fb2c168afd207a
Diyagarg22 <diya.garg_cs24@gla.ac.in>
2025-09-12 11:46:01 +0530

    upload sheets.java

---
+++ sheets.java
@@ -0,0 +1,64 @@
+
+
+
+
+// import java.util.*;
+// public class sheets{
+//     public static int SI ( int p, int r, int t){
+//         int SI=(p*r*t)/100;
+//         return SI;
+
+//     }
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         int p=sc.nextInt();
+//         int r=sc.nextInt();
+//         int t=sc.nextInt();
+// //         int SI= SI(p,r,t);
+// //         System.out.println(SI);
+// //     }
+// // }
+
+
+// import java.util.*;
+// public class sheets{
+//     public static int nos(int n){
+//         int nos= (n*(n-1))/2;
+//         return nos;
+//     }
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         int n=sc.nextInt();
+
+
+//         int nos= nos(n);
+//         System.out.println(nos);
+
+//     }
+// }
+
+// import java.util.*;
+// public class sheets{
+//     public static int nor(int a,int b, int c){
+//         int nor=a+b+c;
+//         return nor;
+//     }
+//     public static void main(String[]args){
+//         Scanner sc=new Scanner(System.in);
+//         int a =sc.nextInt();
+//         int b= sc.nextInt();
+//         int c =sc.nextInt();
+//         int nor=nor(a,b,c);
+//         System.out.println(nor);
+
+//     }
+// }
+import java.util.*;
+public class sheets{
+    
+}
+
+
+
+   
+
