eb52704d5a1a0bc66bed66b17f2aa625150c18d9
Diyagarg22 <diya.garg_cs24@gla.ac.in>
2025-09-12 13:22:30 +0530

    update functions.java

---
+++ functions.java
@@ -0,0 +1,37 @@
+// import java.util.*;
+// public class functions {
+//     public static void printMyName(String name){
+//         System.out.println(name);
+//         return;
+//     }
+//     public static void main(String[]args){
+//         Scanner sc= new Scanner(System.in);
+//         String name = sc.next();
+//         printMyName(name);
+
+
+//     }
+
+
+    
+// // }
+// import java.util.*;
+// public class functions{
+//     public static float calculateSum (float a,float b){
+//         float sum=a+b;
+//         return sum;
+
+//     }
+//     public static void main(String[]args){
+//         Scanner sc =new Scanner(System.in);
+//         float a=sc.nextFloat();
+//         float b=sc.nextFloat();
+//         float sum= calculateSum (a,b);
+//         System.out.println(sum);
+
+
+//     }
+// }
+// 
+// import java.util.*;
+// public class int factorial(int a)
