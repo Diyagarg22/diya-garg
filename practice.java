4010bf8418ca02ec141b497bc950ad7ab1d570d2
Diyagarg22 <diya.garg_cs24@gla.ac.in>
2025-09-12 13:23:20 +0530

    update practice.java

---
+++ practice.java
@@ -0,0 +1,42 @@
+// import java.util.*;
+// public class practice {
+//     public static void main(String[]args){
+//         Scanner sc= new Scanner(System.in);
+//         int r= sc.nextInt();
+//         double area = 3.14*r*r;
+//         System.out.println(area);
+    
+
+//     }
+    
+// // }
+// import java.util.*;
+// public class practice{
+//     public static void main (String[]args){
+//         Scanner sc= new Scanner(System.in);
+//         int n =sc.nextInt();
+//         for(int i=1; i<11; i++){
+//             System.out.println(n*i);
+//         }
+//     }
+
+    
+// // }
+// import java.util.*;
+// public class practice {
+//     public static void main(String[]args){
+//         Scanner sc = new Scanner(System.in);
+//         int n=sc.nextInt();
+//         for(int i=2;i<n ; i=i+2){
+//             System.out.println(i);
+//         }
+
+//     }
+// }
+// import java.util.*;
+//  public class practice {
+//     public static void main(String[]args){
+//         for( ;  ;){
+//             System.out.println("apna collage");
+//         }
+//     } }
