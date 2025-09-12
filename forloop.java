b7e394a383b24b74534aad90d14fdec65354dfbd
Diyagarg22 <diya.garg_cs24@gla.ac.in>
2025-09-12 13:07:06 +0530

    update forloop.java

---
+++ forloop.java
@@ -0,0 +1,41 @@
+public class loops {
+    public static void main(String[]args){
+        for(int counter=0;counter<5;counter+=1){
+            System.out.println("hello world");
+        }
+    }
+    
+}
+public class loops {
+    public static void main(String[]args){
+        for(int counter=0;counter<11;counter++){
+            System.out.println(counter);
+        }
+
+    }
+}
+
+import java.util.*;
+public class forloop{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum=0;
+for(int i=1; i<=n; i++){
+    sum=sum+i;
+    
+    }
+    System.out.println(sum);
+
+}}
+
+import java.util.*;
+public class forloop{
+    public static void main(String[]args){
+        Scanner sc= new Scanner(System.in);
+        int n= sc.nextInt();
+        for(int i=1; i<11;i++){
+            System.out.println(n*i);
+        }
+    }
+}
